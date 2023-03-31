008e3a2f2eb942fd0dbd691afd7e85e32bec3ede
Oskar Daszkowski <oskarrrd53@gmail.com>
2023-03-31 13:11:24 +0200

    refactor the product list component

--- components/ProductsList/ProductsList.jsx
+++ components/ProductsList/ProductsList.jsx
@@ -1,27 +1,12 @@
 import styles from "../../styles/ProductsList.module.css";
-import Link from "next/link";
-import Image from "next/image";
+import ProductItem from "../ProductItem/ProductItem";
 
 const ProductsList = ({ items }) => {
   return (
     <ul className={styles.list__container}>
       {items.map((item, i) => (
         <li className={styles.list__item} key={i}>
-          <div className={styles["list__item-container"]}>
-            <Link className={styles["list__item-link"]} href={`${item.category}/${item.id}`}>
-              <Image
-                src={item.img}
-                width={200}
-                height={200}
-                alt={`${item.title} image`}
-              />
-              <p className={styles["list__item-title"]}>{item.title}</p>
-              <span className={styles["list__item-price"]}>{item.price}zł</span>
-            </Link>
-            <button className={styles["list__item-btn"]}>
-              Dodaj do koszyka
-            </button>
-          </div>
+          <ProductItem item={item} />
         </li>
       ))}
     </ul>
